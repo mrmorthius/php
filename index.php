8156a1ef9d037caac50ae974967c8b876092b77e
Morthius <morten_2104@hotmail.com>
2024-06-19 15:23:54 +0200

    tailwind template implemented

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
 
-$books = [
-  [
-    "name" => "Wester",
-    "author" => "Tommy",
-    "pageUrl" => "www.westerbooks.com",
-    "releaseYear" => 1999
-  ],
-  [
-    "name" => "Spaghetti",
-    "author" => "Jens",
-    "pageUrl" => "www.spaghettibooks.com",
-    "releaseYear" => 2000
-  ]
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-  return strtoupper($book["author"]) === "JENS";
-});
 
 require("index.view.php");
