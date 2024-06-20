e208d19f853d28a1508df66c2a17e9b982e28143
Morthius <morten_2104@hotmail.com>
2024-06-20 21:07:53 +0200

    Added connection info for db

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require('functions.php');
-require("router.php");
+// require("router.php");
+
+
+//Connect to database
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4;user=root'; //connection-string
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
